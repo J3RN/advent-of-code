ca7c11ef2f6a17f4e9ce671bb5472ab811e5374c
Jonathan Arnett <jonarnett90@gmail.com>
2022-12-11 14:42:18 -0500

    Day9p2 Haskell (+ refactor)

--- 2022/day9/Day9.hs
+++ 2022/day9/Day9.hs
@@ -6,13 +6,7 @@ import Text.Printf (printf)
 
 import Data.List (intersect, nub)
 
-import Debug.Trace (trace)
-
 type Pos = (Int, Int)
-
-data PairState = PairState { headPos :: Pos
-                           , tailPos :: Pos
-                           }
 
 data Step
   = R
@@ -25,23 +19,19 @@ main :: IO ()
 main = do
   contents <- readFile "input"
   steps <- either (fail . show) pure $ runParser parseFile () "input" contents
-  let states = scanl applyStep (PairState {headPos = (0, 0), tailPos = (0, 0)}) steps
-      uniquePos = nub . map tailPos $ states
+  let states = scanl moveList [(0,0), (0,0)] steps
+      uniquePos = nub . map (head . reverse) $ states
       numUnique = length uniquePos
-  printf "Unique tail poses: %d\n" numUnique
-
-applyStep :: PairState -> Step -> PairState
-applyStep state step =
-  moveTail (moveHead state step)
+  printf "Unique tail positions: %d\n" numUnique
+  let intermediateLists = scanl moveList (replicate 10 (0, 0)) steps
+      uniquePos' = nub . map (head . reverse) $ intermediateLists
+      numUnique' = length uniquePos'
+  printf "Unique 10' tail positions: %d\n" numUnique'
 
 -- Surrounding also includes the given point
 surrounding :: Pos -> [Pos]
 surrounding (row, col) =
   [(r, c) | r <- [row-1..row+1], c <- [col-1..col+1]]
-
-moveHead :: PairState -> Step -> PairState
-moveHead (PairState hPos tPos) step =
-  (PairState (moveKnot hPos step) tPos)
 
 moveKnot :: Pos -> Step -> Pos
 moveKnot (r, c) R = (r, c + 1)
@@ -54,12 +44,25 @@ diagonals (row, col) =
   [(r,c) | r <- [row-1..row+1], c <- [col-1..col+1], (r /= row && c /= col)]
 
 -- Catch the tail up to the head, if necessary
-moveTail :: PairState -> PairState
-moveTail (PairState hPos@(hRow, hCol) tPos@(tRow, tCol))
-  | hPos `elem` (surrounding tPos) = PairState hPos tPos
-  | hRow /= tRow && hCol == tCol = PairState hPos ((hRow + tRow) `div` 2, hCol)
-  | hRow == tRow && hCol /= tCol = PairState hPos (hRow, (hCol + tCol) `div` 2)
-  | otherwise = PairState hPos (head ((diagonals tPos) `intersect` (surrounding hPos)))
+moveTail :: Pos -> Pos -> Pos
+moveTail hPos@(hRow, hCol) tPos@(tRow, tCol)
+  | hPos `elem` (surrounding tPos) = tPos
+  | hRow /= tRow && hCol == tCol =   ((hRow + tRow) `div` 2, hCol)
+  | hRow == tRow && hCol /= tCol =   (hRow, (hCol + tCol) `div` 2)
+  | otherwise =                      (head ((diagonals tPos) `intersect` (surrounding hPos)))
+
+-- Moves the head using @step@, then iterates the list, using the previous
+-- element to determine the new position of the element
+moveList :: [Pos] -> Step -> [Pos]
+moveList (h:rest) step =
+  let newHead = moveKnot h step
+  in reverse . foldl movePair [newHead] $ rest
+-- Really just here to assuage the compiler
+moveList empty _step = empty
+
+movePair :: [Pos] -> Pos -> [Pos]
+movePair list e = updatedPos:list
+  where updatedPos = moveTail (head list) e
 
 --- Parser
 -- Notably, the parser will expand lines such as "R 2" to [R, R]
