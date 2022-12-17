9c2e4b67fd484868f001487328275c71bbe5b85d
Jonathan Arnett <jonarnett90@gmail.com>
2022-12-17 14:36:59 -0500

    Part 2 is right; Part 1 is not

--- 2022/day15/Day15.hs
+++ 2022/day15/Day15.hs
@@ -1,39 +1,19 @@
--- My original inclination was to iterate over each position and for each
--- position iterate over the list of sensors and determine if the position is
--- closer to the given sensor than any other sensor yet further that than
--- sensor's detected beacon.  I now that this is a terrible idea O(n*s*d) where
--- n is the number of positions (size of grid), s is the number of sensors, and
--- d is the average number of such positions as described above.
-
--- New idea: Populate the grid with `Sensor`, `Beacon`, `Scanned`, and `Unknown`
--- values.  For this, we start with a grid that is totally `Unknown`, and for
--- each sensor we place it, it's beacon, and fill the spaces between the sensor
--- and beacon with `Scanned`. O(s*d + n) (n from the scan of the grid).
-
--- Update: Using an Array to model the grid resulted in a heap overflow.  I
--- think the grid may be sparse (no guarantee), and perhaps using a Set will
--- work better.
-
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
 import Text.Printf (printf)
 
-import Data.Array.IArray
+import Data.List (find, maximumBy, minimumBy, nub)
+import Data.Maybe (isJust)
 
--- Ye olde `Pos` type, this time a proper record instead of a tuple
-data Pos = Pos { x :: Int
-               , y :: Int
-               }
-           deriving (Show, Eq, Ord, Ix)
+import Debug.Trace (trace)
 
-data Cell = Sensor
-          | Beacon
-          | Scanned
-          | Unknown
-          deriving (Show, Eq)
+type Pos = (Int, Int)
 
-type Grid = Array Pos Cell
+data Sensor = Sensor { pos :: Pos
+                     , distanceToBeacon :: Int
+                     }
+              deriving Show
 
 data InputLine = InputLine { sensorPos :: Pos
                            , beaconPos :: Pos
@@ -44,44 +24,72 @@ main :: IO ()
 main = do
   contents <- readFile "input"
   inputLines <- either (fail.show) pure $ runParser parseFile () "input" contents
-  let allPos = (map sensorPos inputLines) ++ (map beaconPos inputLines)
-      minX = minimum . map x $ allPos
-      maxX = maximum . map x $ allPos
-      minY = minimum . map y $ allPos
-      maxY = maximum . map y $ allPos
-      grid = foldl populate (emptyGrid (minX, minY) (maxX, maxY)) inputLines
-      numPotential = length . filter (== Unknown) . map (grid !) $ [Pos {x = thisX, y = 2000000} | thisX <- [minX..maxX]]
-  printf "Num potential at y = 2000000: %d\n" numPotential
+  let sensors = map toSensor inputLines
+      leftMostSensor = minimumBy compareByX sensors
+      rightMostSensor = maximumBy compareByX sensors
+      minX = (fst . pos $ leftMostSensor) - (distanceToBeacon leftMostSensor) - 1
+      maxX = (fst . pos $ rightMostSensor) + (distanceToBeacon rightMostSensor) + 1
+      row = 2000000
+      ns = length . filter (isJust . scannedBy sensors) . map (\x -> (x, row)) $ [minX..maxX]
+      numBeaconsInRow = length . filter (((==) row) . snd) . nub $ map beaconPos inputLines
+  printf "Num scanned in row %d: %d\n" row $ ns - numBeaconsInRow
+  -- let (thisX, thisY) = findUnscanned sensors (maybeNext 0 4000000 sensors) (0, 0)
+  -- printf "Tuning frequency: %d\n" $ thisX * 4000000 + thisY
 
-emptyGrid :: (Int, Int) -> (Int, Int) -> Grid
-emptyGrid (minX, minY) (maxX, maxY) =
-  let values = replicate ((maxX - minX) * (maxY - minY)) Unknown
-  in listArray (Pos {x=minX, y=minY}, Pos {x=maxX, y=maxY}) values
+compareByX :: Sensor -> Sensor -> Ordering
+compareByX s1 s2 = compare (fst . pos $ s1) (fst . pos $ s2)
 
-populate :: Grid -> InputLine -> Grid
-populate grid (InputLine { sensorPos=sp, beaconPos=bp }) =
-  let distance = taxicabDistance sp bp
-      scanned = map (\pos -> (pos, Scanned)) . filter (withinBounds grid) $ scannedPoints distance sp
-  in grid // ([(sp, Sensor), (bp, Beacon)] ++ scanned)
+numScanned :: [Sensor] -> Int -> Pos -> Int -> Int
+numScanned sensors maxX p@(thisX, thisY) num =
+  case nextX p <$> scannedBy sensors p of
+    Just nX
+      | nX <= maxX -> numScanned sensors maxX (nX, thisY) (num + (nX - thisX))
+      | otherwise  -> num + (nX - thisX)
+    Nothing
+      | thisX + 1 <= maxX -> numScanned sensors maxX (thisX + 1,  thisY) num
+      | otherwise         -> num
 
-withinBounds :: Grid -> Pos -> Bool
-withinBounds grid (Pos {x = thisX, y = thisY}) =
-  let (Pos {x = minX, y = minY}, Pos {x = maxX, y = maxY}) = bounds grid
-  in thisX >= minX
-  && thisX <= maxX
-  && thisY >= minY
-  && thisY <= maxY
+findUnscanned :: [Sensor] -> (Pos -> Maybe Pos) -> Pos -> Pos
+findUnscanned sensors nextFun startPos =
+  case nextFun startPos of
+    Just np -> findUnscanned sensors nextFun np
+    Nothing -> startPos
 
-scannedPoints :: Int -> Pos -> [Pos]
-scannedPoints distance sp@(Pos {x = sensorX, y = sensorY}) =
-  [Pos {x = scanX, y = scanY} |
-    scanX <- [sensorX - distance .. sensorX + distance],
-    scanY <- [sensorY - distance .. sensorY + distance],
-    (taxicabDistance sp Pos {x = scanX, y = scanY}) <= distance]
+maybeNext :: Int -> Int -> [Sensor] -> Pos -> Maybe Pos
+maybeNext minX maxX sensors thisPos =
+  (nextPos minX maxX thisPos) <$> (scannedBy sensors thisPos)
+
+nextX :: Pos -> Sensor -> Int
+nextX p sensor@Sensor {pos = (xs, _ys)} =
+  let dX = deltaX p sensor
+  in xs + dX + 1
+
+nextPos :: Int -> Int -> Pos -> Sensor -> Pos
+nextPos minX maxX p@(_thisX, thisY) sensor@(Sensor {pos = (xs, _ys)}) =
+  let nX = nextX p sensor
+  in if nX > maxX
+     then (minX, thisY + 1)
+     else (nX, thisY)
+
+deltaX :: Pos -> Sensor -> Int
+deltaX (_thisX, thisY) (Sensor {pos = (_xs, ys), distanceToBeacon = d}) =
+  d - (abs (thisY - ys))
+
+scannedBy :: [Sensor] -> Pos -> Maybe Sensor
+scannedBy sensors p =
+  find (not . beyondScan p) sensors
+
+toSensor :: InputLine -> Sensor
+toSensor (InputLine {sensorPos = sp, beaconPos = bp}) =
+  Sensor {pos = sp, distanceToBeacon = (taxicabDistance sp bp)}
 
 taxicabDistance :: Pos -> Pos -> Int
-taxicabDistance (Pos {x=x1, y=y1}) (Pos {x=x2, y=y2}) =
+taxicabDistance (x1, y1) (x2, y2) =
   (abs (x1 - x2)) + (abs (y1 - y2))
+
+beyondScan :: Pos -> Sensor -> Bool
+beyondScan p sensor =
+  taxicabDistance p (pos sensor) > distanceToBeacon sensor
 
 -- Parser -- Not too bad; could've just done string splitting honestly
 
@@ -102,7 +110,7 @@ parsePos = do
   parsedX <- parseNumber
   _ <- string ", y="
   parsedY <- parseNumber
-  return Pos { x = parsedX, y = parsedY }
+  return (parsedX, parsedY)
 
 parseNumber :: Parser Int
 parseNumber = do
