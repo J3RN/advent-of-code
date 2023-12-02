5ae68770e4a7efc77651c0c7da01b0e45bdb008b
Jonathan Arnett <jonarnett90@gmail.com>
2023-12-02 01:45:33 -0500

    Oh dear god, isPrefixOf exists

--- 2023/day1/Day1.hs
+++ 2023/day1/Day1.hs
@@ -1,6 +1,6 @@
 import Control.Applicative ((<|>), liftA2)
 
-import Data.List (tails)
+import Data.List (isPrefixOf, tails)
 
 main :: IO ()
 main = do
@@ -27,30 +27,26 @@ parseDigit = liftA2 (<|>) parseLiteralDigit parseWordDigit
 
 parseLiteralDigit :: String -> Maybe Int
 parseLiteralDigit str
-    | beginsWith str "1"     = Just 1
-    | beginsWith str "2"     = Just 2
-    | beginsWith str "3"     = Just 3
-    | beginsWith str "4"     = Just 4
-    | beginsWith str "5"     = Just 5
-    | beginsWith str "6"     = Just 6
-    | beginsWith str "7"     = Just 7
-    | beginsWith str "8"     = Just 8
-    | beginsWith str "9"     = Just 9
+    |  "1" `isPrefixOf` str  = Just 1
+    |  "2" `isPrefixOf` str  = Just 2
+    |  "3" `isPrefixOf` str  = Just 3
+    |  "4" `isPrefixOf` str  = Just 4
+    |  "5" `isPrefixOf` str  = Just 5
+    |  "6" `isPrefixOf` str  = Just 6
+    |  "7" `isPrefixOf` str  = Just 7
+    |  "8" `isPrefixOf` str  = Just 8
+    |  "9" `isPrefixOf` str  = Just 9
     | otherwise              = Nothing
 
 parseWordDigit :: String -> Maybe Int
 parseWordDigit str
-    | beginsWith str "one"   = Just 1
-    | beginsWith str "two"   = Just 2
-    | beginsWith str "three" = Just 3
-    | beginsWith str "four"  = Just 4
-    | beginsWith str "five"  = Just 5
-    | beginsWith str "six"   = Just 6
-    | beginsWith str "seven" = Just 7
-    | beginsWith str "eight" = Just 8
-    | beginsWith str "nine"  = Just 9
-    | otherwise              = Nothing
-
-beginsWith :: String -> String -> Bool
-beginsWith base comp =
-  take (length comp) base == comp
+    | "one"   `isPrefixOf` str = Just 1
+    | "two"   `isPrefixOf` str = Just 2
+    | "three" `isPrefixOf` str = Just 3
+    | "four"  `isPrefixOf` str = Just 4
+    | "five"  `isPrefixOf` str = Just 5
+    | "six"   `isPrefixOf` str = Just 6
+    | "seven" `isPrefixOf` str = Just 7
+    | "eight" `isPrefixOf` str = Just 8
+    | "nine"  `isPrefixOf` str = Just 9
+    | otherwise                = Nothing
