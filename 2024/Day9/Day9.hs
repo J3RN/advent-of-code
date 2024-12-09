b7a6945a3c050f1e9e22aedb81ff38a34a6f2755
Jonathan Arnett <jonathan.arnett@proton.me>
2024-12-09 18:50:03 -0500

    WIP 2024 Day 9 Haskell Part 2

--- 2024/Day9/Day9.hs
+++ 2024/Day9/Day9.hs
@@ -9,8 +9,16 @@ import           Text.Parsec
 main :: IO ()
 main = do
   sectorList <- parseFromFile parseFile "input" >>= either (fail . show) pure
-  let sectors = Array.listArray (0, length sectorList - 1) sectorList
-  print . checksum . compact $ sectors
+  let expandedSectorList = expand sectorList
+  print . checksum . compact . listToArr . expand $ sectorList
+  print . checksum . _ . contiguousCompact . listToArr $ sectorList
+
+listToArr :: [a] -> Array Int a
+listToArr l = Array.listArray (0, length l - 1) l
+
+-- Convert the compact format to the expanded format
+expand ::[(Int, Maybe Integer)] -> [Maybe Integer]
+expand = concatMap (uncurry replicate)
 
 checksum :: Array Int (Maybe Integer) -> Integer
 checksum sectors = foldl u 0 (Array.assocs sectors)
@@ -19,7 +27,8 @@ checksum sectors = foldl u 0 (Array.assocs sectors)
                               Nothing -> acc
 
 compact :: Array Int (Maybe Integer) -> Array Int (Maybe Integer)
-compact sectors = sectors // uncurry (compactUpdates sectors) (Array.bounds sectors)
+compact sectors = sectors // updates
+  where updates = uncurry (compactUpdates sectors) (Array.bounds sectors)
 
 compactUpdates :: Array Int (Maybe Integer) -> Int -> Int -> [(Int, Maybe Integer)]
 compactUpdates sectors lPtr rPtr
@@ -29,6 +38,33 @@ compactUpdates sectors lPtr rPtr
                   Nothing -> case sectors ! rPtr of
                                Just sub -> [(lPtr, Just sub), (rPtr, Nothing)] ++ compactUpdates sectors (lPtr + 1) (rPtr - 1)
                                Nothing  -> compactUpdates sectors lPtr (rPtr - 1)
+
+-- This foldr is going to be hellaceously slow, but we need iterative updates to the sectors arr to
+-- prevent double-allocating a slot
+contiguousCompact :: Array Int (Int, Maybe Integer) -> Array Int (Int, Maybe Integer)
+contiguousCompact sectors = foldr contiguousCompact' sectors (Array.assocs sectors)
+
+contiguousCompact' :: (Int, (Int, Maybe Integer)) -> Array Int (Int, Maybe Integer) -> Array Int (Int, Maybe Integer)
+contiguousCompact' (_, (_, Nothing)) sectors = sectors
+contiguousCompact' (ix, (c, val)) sectors    = case findSizeSlot c sectors of
+                                                 (Just (ix2, (c2, _))) | ix2 < ix -> sectors // _
+                                                 Nothing    -> sectors
+  where
+    findSizeSlot :: Int -> Array Int (Int, Maybe Integer) -> Maybe (Int, (Int, Maybe Integer))
+    findSizeSlot c = List.find ((>= c) . fst) . Array.assocs
+
+
+-- THIS DOESN'T WORK BECAUSE WE DON'T GO LINEARLY LTR
+-- contiguousCompact :: Array Int (Int, Maybe Integer) -> Array Int (Int, Maybe Integer)
+-- contiguousCompact sectors = sectors // updates
+--   where updates = uncurry (contiguousCompact' sectors) (first (+ 1) $ Array.bounds sectors)
+
+-- contiguousCompact' :: Array Int (Int, Maybe Integer) -> Int -> Int -> [(Int, (Int, Maybe Integer))]
+-- contiguousCompact' sectors lPtr rPtr
+--   | lPtr >= rPtr = []
+--   | otherwise = case sectors ! rPtr of
+--                   (_, Nothing)     -> contiguousCompact' sectors lPtr (rPtr -1)
+--                   (c, Just nodeId) -> _
 
 -- Parser
 
@@ -48,13 +84,13 @@ parseFromFile p fname
          return (runP p initialState fname input)
 
 -- Parses the format used in the example, with `Nothing` representing free space
-parseFile :: Parser [Maybe Integer]
-parseFile = concat <$> manyTill parseSector (char '\n')
+parseFile :: Parser [(Int, Maybe Integer)]
+parseFile = manyTill parseSector (char '\n')
 
-parseSector :: Parser [Maybe Integer]
+parseSector :: Parser (Int, Maybe Integer)
 parseSector = do
   ParseState isFree nodeId <- getState
   c <- read . List.singleton <$> digit
-  let res = if isFree then replicate c Nothing else replicate c (Just nodeId)
+  let res = if isFree then (c, Nothing) else (c, Just nodeId)
   putState (ParseState (not isFree) (if isFree then nodeId + 1 else nodeId))
   return res
