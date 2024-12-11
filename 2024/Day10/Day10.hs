b4e124789ab112b093bd502228c1c1c4a0d12278
Jonathan Arnett <jonathan.arnett@proton.me>
2024-12-11 00:27:50 -0500

    2024 Day 10 Haskell
    I don't feel great about these, but they are short and do work

--- 2024/Day10/Day10.hs
+++ 2024/Day10/Day10.hs
@@ -1,5 +1,7 @@
 import           Data.Bifunctor   (first)
-import           Data.Map         (Map)
+import           Data.Functor     (($>))
+import qualified Data.List        as List
+import           Data.Map         (Map, (!?))
 import qualified Data.Map         as Map
 import           Text.Parsec
 import           Text.Parsec.Text (Parser, parseFromFile)
@@ -12,10 +14,26 @@ main :: IO ()
 main = do
   topology <- parseFromFile parseFile "input" >>= either (fail . show) pure
   print . sum . trailheadCounts $ topology
+  print . sum . trailRatings $ topology
 
 trailheadCounts :: TopologyMap -> [Int]
-trailheadCounts topology = map (trailheadCount topology) (Map.assocs topology)
-  where trailheadCount = _
+trailheadCounts topology = map (trailheadCount 1 . fst) $ filter ((== 0) . snd) $ Map.assocs topology
+  where trailheadCount :: Height -> Point -> Int
+        trailheadCount height point = length $ List.nub $ trailheadsFrom 1 point
+        trailheadsFrom :: Height -> Point -> [Point]
+        trailheadsFrom 9      point = filter (\neighbor -> topology !? neighbor == Just 9) $ neighbors point
+        trailheadsFrom height point = concatMap (trailheadsFrom (height + 1)) $ filter (\neighbor -> topology !? neighbor == Just height) $ neighbors point
+
+trailRatings :: TopologyMap -> [Int]
+trailRatings topology = map (trailCount 1 . fst) $ filter ((== 0) . snd) $ Map.assocs topology
+  where trailCount :: Height -> Point -> Int
+        trailCount height point = length . List.nub $ trails height point
+        trails :: Height -> Point -> [[Point]]
+        trails 9      point = map ((point:) . List.singleton) $ filter (\neighbor -> topology !? neighbor == Just 9) $ neighbors point
+        trails height point = concatMap (map (point:) . trails (height + 1)) $ filter (\neighbor -> topology !? neighbor == Just height) $ neighbors point
+
+neighbors :: Point -> [Point]
+neighbors (x, y) = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
 
 -- Parser (not stateful today, though it could be!)
 
@@ -24,4 +42,4 @@ parseFile = Map.fromList . concat . zipWith applyRowIx [0..] <$> manyTill parseR
   where applyRowIx rowIx = map (first (,rowIx))
 
 parseRow :: Parser [(Int, Height)]
-parseRow = zip [0..] <$> many1 (read . pure <$> digit)  <* char '\n'
+parseRow = zip [0..] <$> many1 ((read . pure <$> digit) <|> (char '.' $> (-1)))  <* char '\n'
