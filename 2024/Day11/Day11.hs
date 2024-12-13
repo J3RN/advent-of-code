b5ab1b2eb8c85ec16e04d3eb282a1dc55c6aa0cb
Jonathan Arnett <jonathan.arnett@proton.me>
2024-12-13 11:12:38 -0500

    Attempt to finish Day11 with memoization (doesn't work)

--- 2024/Day11/Day11.hs
+++ 2024/Day11/Day11.hs
@@ -1,19 +1,36 @@
-import qualified Data.List   as List
-import           Debug.Trace (traceShowId)
-import           GHC.Num     (integerLogBase)
+import           Control.Monad       (when)
+import           Control.Monad.State
+import           Data.Maybe          (fromMaybe)
+-- import qualified Data.List           as List
+import           Control.Applicative (liftA2)
+import           Data.Map.Strict     (Map, (!?))
+import qualified Data.Map.Strict     as Map
+import           Debug.Trace         (traceShowId)
+import           GHC.Num             (integerLogBase)
+
+type MemoCache = Map (Int, Integer) Integer
 
 main :: IO ()
 main = do
   initialStones <- map read . words <$> readFile "input"
-  print . sum . map (lengthAfterBlinks 25) $ initialStones
-  print . sum . map (traceShowId . lengthAfterBlinks 75) $ initialStones
+  print . sum . map (\x -> evalState (lengthAfterBlinks 25 x) Map.empty) $ initialStones
+  print . sum . map (\x -> evalState (lengthAfterBlinks 75 x) Map.empty) $ initialStones
 
-lengthAfterBlinks :: Int -> Integer -> Integer
-lengthAfterBlinks 0 _ = 1
-lengthAfterBlinks blinks 0 = lengthAfterBlinks (blinks - 1) 1
-lengthAfterBlinks blinks s | even (numDigits s) = let (before, after) = splitNum s
-                                                   in lengthAfterBlinks (blinks - 1) before + lengthAfterBlinks (blinks - 1) after
-lengthAfterBlinks blinks s = lengthAfterBlinks (blinks - 1) (s * 2024)
+lengthAfterBlinks :: Int -> Integer -> State MemoCache Integer
+lengthAfterBlinks n s = fromMaybe <$> (lengthAfterBlinks' n s >>= cacheRes (n, s)) <*> gets (!? (n, s))
+  where cacheRes key res = do
+          when (snd key < 10) (modify (Map.insert key res))
+          return res
+
+lengthAfterBlinks' :: Int -> Integer -> State MemoCache Integer
+lengthAfterBlinks' 0 _ = return 1
+lengthAfterBlinks' blinks 0 = lengthAfterBlinks (blinks - 1) 1
+lengthAfterBlinks' blinks s | even (numDigits s) = do
+                                let (before, after) = splitNum s
+                                beforeRes <- lengthAfterBlinks (blinks - 1) before
+                                afterRes <- lengthAfterBlinks (blinks - 1) after
+                                return (beforeRes + afterRes)
+lengthAfterBlinks' blinks s = lengthAfterBlinks (blinks - 1) (s * 2024)
 
 -- blink :: [Integer] -> [Integer]
 -- blink = concatMap blinkStone
